ae626d8326c110d8e6cd25b8bdc0d8a9755a850a
Marcel Correa <cecel85@gmail.com>
2024-05-22 15:50:33 -0300

    estruturas de repeticao

---
+++ src/ExemploForArray.java
@@ -0,0 +1,12 @@
+// ExemploForArray.java
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+        
+            //Forma abreviada
+        for(String aluno : alunos) {
+          System.out.println("Nome do aluno é: " + aluno);
+        }
+    
+    }
+    }
